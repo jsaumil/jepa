bd9e5353abe16ea6ce1b8f4ffba618f0f7616e63
Saumil Jain <156582131+jsaumil@users.noreply.github.com>
2026-05-08 23:20:29 +0530

    Updated files

--- utils/pos_embs.py
+++ utils/pos_embs.py
@@ -9,7 +9,7 @@ def get_1d_sincos_pos_embed_from_grid(embed_dim, pos):
     assert embed_dim % 2 == 0
     omega = np.arange(embed_dim // 2, dtype=float)
     omega /= embed_dim / 2.
-    
+    omega = 1. / 10000
 
 def get_1d_sincos_pos_embed():
     pass
