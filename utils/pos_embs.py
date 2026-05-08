1fdc505f432f6c9027e75775a8cc2ce645978825
Saumil Jain <156582131+jsaumil@users.noreply.github.com>
2026-05-09 00:09:17 +0530

    Updated files

--- utils/pos_embs.py
+++ utils/pos_embs.py
@@ -38,4 +38,5 @@ def get_2d_sincos_pos_embed(embed_dim, grid_size, cls_token=False):
     grid_w = np.arange(grid_size, dtype=float)
     grid_w, grid_h = np.meshgrid(grid_w, grid_h) # order of meshgrid is very important for indexing as [h, w]
 
-    emb_h = get
+    emb_h = get_1d_sincos_pos_embed_from_grid(embed_dim//2, grid_h)
+    emb_w = get_1d_sincos_pos_embed_from_grid(embed_dim//2, )
