ba63bbb8a8cf0ca988ca0c6a1381fecbabec9b0c
Saumil Jain <156582131+jsaumil@users.noreply.github.com>
2026-05-09 00:08:03 +0530

    Updated files

--- utils/pos_embs.py
+++ utils/pos_embs.py
@@ -36,4 +36,4 @@ def get_1d_sincos_pos_embed(embed_dim, grid_size, cls_token=False):
 def get_2d_sincos_pos_embed(embed_dim, grid_size, cls_token=False):
     grid_h = np.arange(grid_size, dtype=float)
     grid_w = np.arange(grid_size, dtype=float)
-    grid_w, grid_h
+    grid_w, grid_h = np.meshgrid(grid_w, grid_h) # order of meshgrid is very 
