72a7fde4437bf4a7ab8cd18f41df4d30fbb25483
Saumil Jain <156582131+jsaumil@users.noreply.github.com>
2026-05-09 00:07:27 +0530

    Updated files

--- utils/pos_embs.py
+++ utils/pos_embs.py
@@ -35,4 +35,5 @@ def get_1d_sincos_pos_embed(embed_dim, grid_size, cls_token=False):
 
 def get_2d_sincos_pos_embed(embed_dim, grid_size, cls_token=False):
     grid_h = np.arange(grid_size, dtype=float)
-    grid_w = 
+    grid_w = np.arange(grid_size, dtype=float)
+    grid_w, grid_h
