864b7f5548c9d96fee06fedf8250a9e390390809
Saumil Jain <156582131+jsaumil@users.noreply.github.com>
2026-05-08 23:18:02 +0530

    Updated files

--- utils/pos_embs.py
+++ utils/pos_embs.py
@@ -1,6 +1,9 @@
 import numpy as np
 
-def get_1d_sincos_pos_embed_from_grid(embed)
+def get_1d_sincos_pos_embed_from_grid(embed_dim, pos):
+    """
+    embed_dim: ouput dimension for each 
+    """
 
 def get_1d_sincos_pos_embed():
     pass
