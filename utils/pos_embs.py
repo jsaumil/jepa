b3823e0f6b669ad80bf58f8e0a77f4511c111957
Saumil Jain <156582131+jsaumil@users.noreply.github.com>
2026-05-08 23:19:15 +0530

    Updated files

--- utils/pos_embs.py
+++ utils/pos_embs.py
@@ -4,8 +4,10 @@ def get_1d_sincos_pos_embed_from_grid(embed_dim, pos):
     """
     embed_dim: ouput dimension for each position
     pos: a list of positions to be encoded: size (M,)
-    returns: (M,D)
+    returns: (M, D)
     """
+    assert embed_dim % 2 == 0
+    omega = np.arange(embed)
 
 def get_1d_sincos_pos_embed():
     pass
